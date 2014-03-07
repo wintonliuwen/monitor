e1a19c9bcb386a58bd7a30c2fae4a06b9a83cde5
winton.liu <newtonliuwen@gmail.com>
2014-03-07 18:24:58 +0800

    Add CPU usage processing
    Signed-off-by: winton.liu <newtonliuwen@gmail.com>

--- monitor.sh
+++ monitor.sh
@@ -22,11 +22,30 @@ processmem(){
 	local result=""
 	pid=$(pidof "$1")
 	if [ -n "$pid" ];then
-		result=$(echo $(cat /proc/${pid}/status | grep VmSize | awk -F':' '{print $2}'))
+		result="$(echo $(cat /proc/${pid}/status | grep VmSize | awk -F':' '{print $2}'))"
 	else
 		result="No such process:$1"
 	fi
-	print_result "$result"
+	print_result "${result}"
 }
 
+# CPU parts
+processcpu(){
+	local totalcpu=""
+	local processcpu=0
+	local result=0
+	pid=$(pidof "$1")
+	if [ -n "$pid" ];then
+		totalcpu=$(cat /proc/stat|grep "cpu "|awk '{for(i=2;i<=NF;i++)j+=$i;print j;}')
+		processcpu=$(cat /proc/${pid}/stat | awk '{print $14+$15+$16+$17}')
+		result=$((processcpu * 100 / totalcpu))
+		result="${result}%"
+	else
+		result="No such process:$1"
+	fi
+	print_result "${result}"
+}
+
+
+processcpu $1
 processmem $1
